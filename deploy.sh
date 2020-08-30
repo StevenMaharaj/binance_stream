bd0983307c9aa768c9f9a8018bbc6a48aae2d291
StevenMaharaj <stevesamah@gmail.com>
2020-08-30 14:47:08 +1000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,11 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:StevenMaharaj/binance_stream.git master:gh-pages
+git push -f git@github.com:StevenMaharaj/binance_stream.git master:gh-pages
 # git branch -M master
 # git remote add origin https://github.com/StevenMaharaj/binance_stream.git
 # git push -u origin master
 
-git branch -M deploy
-git remote add origin https://github.com/StevenMaharaj/binance_stream.git
-git push -u origin deploy
+# git branch -M deploy
+# git remote add origin https://github.com/StevenMaharaj/binance_stream.git
+# git push -u origin deploy
