24a625888faceaf0a1b3b1efa32cb479eef7556c
StevenMaharaj <stevesamah@gmail.com>
2020-08-30 14:34:21 +1000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-yarn build
+# yarn build/
 
 # navigate into the build output directory
-# cd dist
+cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -20,4 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:StevenMaharaj/binance_stream.git master:gh-pages
+# git push -f git@github.com:StevenMaharaj/binance_stream.git master:gh-pages
+git branch -M master
+git remote add origin https://github.com/StevenMaharaj/binance_stream.git
+git push -u origin master
